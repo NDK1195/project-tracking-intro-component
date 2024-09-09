b851179e0a1d9d2ef4e7cf176bada213dbd1bb7d
ndkhoa1195 <ndkhoa1195@gmail.com>
2024-09-09 14:52:15 +0700

    finish project

--- src/App.jsx
+++ src/App.jsx
@@ -3,51 +3,54 @@ import illustration from "./images/illustration-devices.svg";
 
 function App() {
   return (
-    <div className="pb-[92px] pt-[38px] xl:pb-[125px] xl:pt-16">
-      <NavBar />
+    <>
+      <div className="absolute right-0 top-0 -z-10 min-h-[425px] min-w-[191px] rounded-bl-[60px] bg-light-grayish-blue lg:min-w-[705px]"></div>
+      <div className="pb-[92px] pt-[38px] xl:pb-[125px] xl:pt-16">
+        <NavBar />
 
-      <main className="3xl:justify-center mt-16 flex flex-col items-center gap-[87px] xl:mt-32 xl:flex-row-reverse">
-        {/* illustration */}
-        <div className="3xl:mr-0 -mr-[139px] min-w-[482px] max-w-[916px] 2xl:-mr-[268px] 2xl:min-w-[916px]">
-          <img
-            src={illustration}
-            alt="illustration devices"
-            className="w-full"
-          />
-        </div>
-        {/* illustration */}
-
-        {/* content */}
-        <div className="max-w-[470px] px-8 text-very-dark-blue 2xl:px-0">
-          <p className="mb-4 flex items-center gap-4 text-[15px] leading-none">
-            <span className="max-h-[25px] max-w-12 rounded-[12.5px] bg-very-dark-blue px-[11px] py-[5px] font-bold tracking-[1px] text-white">
-              NEW
-            </span>
-            <span className="tracking-[5px] opacity-50">
-              MONOGRAPH DASHBOARD
-            </span>
-          </p>
-
-          <h1 className="mb-4 text-[40px] font-bold leading-none xl:mb-8 xl:text-[64px]">
-            POWERFUL INSIGHTS INTO YOUR TEAM
-          </h1>
-
-          <p className="mb-7 max-w-[268px] font-barlow text-xl leading-[26px] opacity-75 xl:mb-16">
-            Project planning and time tracking for agile teams
-          </p>
-
-          <div className="flex max-w-[381px] items-center gap-5 text-sm leading-none xl:gap-6">
-            <button className="rounded-[5px] bg-red px-6 py-3 font-bold tracking-[0.933px] text-white transition-opacity hover:opacity-[0.2473] xl:px-8 xl:py-[14px] xl:text-base xl:tracking-[1.067px]">
-              SCHEDULE A DEMO
-            </button>
-            <p className="tracking-[2.5px] opacity-50 xl:text-[15px] xl:tracking-[5px]">
-              TO SEE A PREVIEW
-            </p>
+        <main className="mt-16 flex flex-col items-center gap-[87px] overflow-hidden xl:mt-32 xl:flex-row-reverse xl:gap-0 2xl:gap-[160px] 3xl:justify-center">
+          {/* illustration */}
+          <div className="-mr-[139px] min-w-[482px] max-w-[916px] 2xl:-mr-[268px] 2xl:min-w-[916px] 3xl:mr-0">
+            <img
+              src={illustration}
+              alt="illustration devices"
+              className="w-full"
+            />
           </div>
-        </div>
-        {/* content */}
-      </main>
-    </div>
+          {/* illustration */}
+
+          {/* content */}
+          <div className="max-w-[470px] px-8 text-very-dark-blue 2xl:px-0">
+            <p className="mb-4 flex items-center gap-4 text-[15px] leading-none">
+              <span className="max-h-[25px] max-w-12 rounded-[12.5px] bg-very-dark-blue px-[11px] py-[5px] font-bold tracking-[1px] text-white">
+                NEW
+              </span>
+              <span className="tracking-[5px] opacity-50">
+                MONOGRAPH DASHBOARD
+              </span>
+            </p>
+
+            <h1 className="mb-4 text-[40px] font-bold leading-none xl:mb-8 xl:text-[64px]">
+              POWERFUL INSIGHTS INTO YOUR TEAM
+            </h1>
+
+            <p className="mb-7 max-w-[268px] font-barlow text-xl leading-[26px] opacity-75 xl:mb-16">
+              Project planning and time tracking for agile teams
+            </p>
+
+            <div className="flex max-w-[381px] items-center gap-5 text-sm leading-none xl:gap-6">
+              <button className="rounded-[5px] bg-red px-6 py-3 font-bold tracking-[0.933px] text-white transition-opacity hover:opacity-[0.2473] xl:px-8 xl:py-[14px] xl:text-base xl:tracking-[1.067px]">
+                SCHEDULE A DEMO
+              </button>
+              <p className="tracking-[2.5px] opacity-50 xl:text-[15px] xl:tracking-[5px]">
+                TO SEE A PREVIEW
+              </p>
+            </div>
+          </div>
+          {/* content */}
+        </main>
+      </div>
+    </>
   );
 }
 export default App;
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,4 +1,105 @@
+import logo from "./images/logo.svg";
+import iconHamburger from "./images/icon-hamburger.svg";
+import iconClose from "./images/icon-close.svg";
+import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 function NavBar() {
-  return <div>NavBar</div>;
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
+
+  return (
+    <>
+      {/* overlay */}
+      {isOpenMenu && (
+        <div
+          className="absolute left-0 top-0 z-10 min-h-dvh w-full bg-transparent xl:hidden"
+          onClick={() => setIsOpenMenu(false)}
+        ></div>
+      )}
+      {/* overlay */}
+      <nav className="relative mx-auto flex max-w-[1110px] items-center justify-between px-8 xl:px-0">
+        <div className="w-6 lg:w-8">
+          <img src={logo} alt="logo" className="w-full" />
+        </div>
+
+        <div>
+          {/* mobile */}
+          <div className="xl:hidden">
+            <div
+              className="w-6 cursor-pointer"
+              onClick={() => setIsOpenMenu(!isOpenMenu)}
+            >
+              <img
+                src={isOpenMenu ? iconClose : iconHamburger}
+                alt="icon menu"
+                className="w-full cursor-pointer"
+              />
+            </div>
+
+            <AnimatePresence>
+              {isOpenMenu && (
+                <motion.ul
+                  className={
+                    "absolute right-8 top-[62px] z-20 flex w-[311px] flex-col items-center gap-6 bg-white p-6 text-lg font-bold leading-none tracking-[1.2px] shadow-modal"
+                  }
+                  initial={{ opacity: 0, y: -50 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: -50 }}
+                >
+                  <li>
+                    <a href="#" className="transition-all hover:underline">
+                      PRODUCT
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="transition-all hover:underline">
+                      FEATURES
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="transition-all hover:underline">
+                      PRICING
+                    </a>
+                  </li>
+                  <li className="h-[1px] w-full bg-very-dark-blue opacity-15"></li>
+                  <li className="opacity-50">
+                    <a href="#" className="transition-all hover:underline">
+                      LOGIN
+                    </a>
+                  </li>
+                </motion.ul>
+              )}
+            </AnimatePresence>
+          </div>
+          {/* mobile */}
+
+          {/* desktop */}
+          <ul className="hidden min-w-[379px] items-center justify-between text-[15px] font-bold leading-none tracking-[1px] text-very-dark-blue xl:flex">
+            <li>
+              <a href="#" className="transition-all hover:underline">
+                PRODUCT
+              </a>
+            </li>
+            <li>
+              <a href="#" className="transition-all hover:underline">
+                FEATURES
+              </a>
+            </li>
+            <li>
+              <a href="#" className="transition-all hover:underline">
+                PRICING
+              </a>
+            </li>
+            <li className="size-[6px] rounded-full bg-very-dark-blue opacity-15"></li>
+            <li className="opacity-50">
+              <a href="#" className="transition-all hover:underline">
+                LOGIN
+              </a>
+            </li>
+          </ul>
+          {/* desktop */}
+        </div>
+      </nav>
+    </>
+  );
 }
 export default NavBar;
